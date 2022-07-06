7e44b1f919f190e1dd2c45124ad23db955263ba9
Alves1Emanuel <emanueellgomes@gmail.com>
2022-07-06 16:20:32 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -0,0 +1,27 @@
+package com.example.soccernews.ui.news;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.soccernews.domain.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsViewModel extends ViewModel {
+
+    private final MutableLiveData<List<News>> news;
+
+    public NewsViewModel() {
+        news = new MutableLiveData<>();
+
+        List<News> news = new ArrayList<>();
+        news.add(new News(tile: "", description:"");
+
+    }
+
+    public LiveData<List<News>> getNews() {
+        return news;
+    }
+}
